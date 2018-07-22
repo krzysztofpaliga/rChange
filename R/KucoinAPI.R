ce412b12ade2bfd3ad7aa32eaedfa565622a3e01
krzychu paliga <paligakrzychu@gmail.com>
2018-07-22 12:17:03 +0200

    minor refactor, adds few basic tests

--- R/KucoinAPI.R
+++ R/KucoinAPI.R
@@ -1,18 +1,18 @@
-source("RChangeInternals.R")
+source("R/rChangeInternals.R")
 
 initKucoinAPI <- function() {
   RChange <- initRChange(baseUrl = "https://api.kucoin.com")
- 
+
   kucoinAPI <- list()
-  
+
   kucoinAPI$listCoins <- function() {
     return (RChange$api$generic(urlEndpointPart = "/v1/market/open/coins"))
   }
-  
+
   kucoinAPI$listTradingSymbolsTick <- function(market = "BTC") {
     return (RChange$api$generic(urlEndpointPart = "/v1/market/open/symbols"))
   }
-  
+
   kucoinAPI$recentlyDealOrders <- function (symbol = "KCS-ETH") {
     return (RChange$api$generic(urlEndpointPart = "/v1/open/deal-orders"))
   }
---
+++ R/rChangeInternals.R
@@ -0,0 +1,64 @@
+require("httr")
+require("jsonlite")
+
+initrChange <- function(baseUrl) {
+  # rChange Base
+  rChange <- list()
+  rChange$base <- baseUrl
+
+  #rChange.endpoints.listTradingMarkets <- list(name = "listTradingMarkets", urlPart = "/v1/open/markets")
+
+  # rChange Shared
+  rChange$shared <- list()
+
+  rChange$shared$getCallingFunctionsName <- function(level = 1) {
+    return (gsub(".*\\.", "", sys.calls()[[sys.nframe()-level]]))
+  }
+
+  rChange$shared$getCallingFunctionsParameters <- function(level = 1) {
+    parameters <- as.list(parent.frame(n = level))
+    return (parameters)
+  }
+
+  rChange$shared$getUrl <- function(urlEndpointPart, parameters = NULL) {
+    urlParametersPart <- rChange$shared$getParametersUrlPart(parameters)
+    return (paste(rChange$base, urlEndpointPart, urlParametersPart, sep=""))
+  }
+
+  rChange$shared$getParametersUrlPart <- function(parameters = NULL) {
+    if (is.null(parameters) || length(parameters) == 0) {
+      return ("")
+    }
+    parametersUrlPart <- "?"
+    parameterNames <- names(parameters)
+    for (i in 1:length(parameterNames)) {
+      sep <- ""
+      if (i > 1) {
+        sep <- "&"
+      }
+      parameterName = parameterNames[i]
+      parameterValue = get(parameterName, parameters)
+      parametersUrlPart <- paste(parametersUrlPart, sep, parameterName, "=", parameterValue, sep="")
+    }
+    return (parametersUrlPart)
+  }
+
+  # rChange API Internals
+  rChange$api <- list()
+
+  rChange$api$generic <- function(urlEndpointPart) {
+    parameters <- rChange$shared$getCallingFunctionsParameters(level = 2)
+    url <- rChange$shared$getUrl(urlEndpointPart = urlEndpointPart, parameters = parameters)
+    return (rChange$api$request(url = url))
+  }
+
+  rChange$api$request <- function(url) {
+    message(paste("GET", url))
+    response.raw <- GET(url)
+    response.content.raw <- content(response.raw, "text")
+    response.content.parsed <- fromJSON(response.content.raw, flatten = TRUE)
+    return(response.content.parsed)
+  }
+
+  return (rChange)
+}
