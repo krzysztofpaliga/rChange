a5966ec937a9820b28c87a465fd0c17f51e049f3
krzychu paliga <paligakrzychu@gmail.com>
2018-08-06 10:13:33 +0200

    test

--- R/Kucoin.R
+++ R/Kucoin.R
@@ -111,7 +111,9 @@ initKucoin <- function(kucoinAPI) {
           } else {
             if (addNewest) {
               #TODO: calculate from on the basis of difference between consecutive date entries (delta ts)
-              if (length(allCoinsHistory[allCoinsHistory$cc == coin,]) == 0) {
+              print(allCoinsHistory[allCoinsHistory$cc == coin,]$date)
+              print(max(allCoinsHistory[allCoinsHistory$cc == coin,]$date))
+              if (length(allCoinsHistory[allCoinsHistory$cc == coin,]$date) == 0) {
                 missingData <- kucoin$getAllHistorical(cryptoCurrency = coin,
                                                        baseCurrency = market,
                                                        type = type)
